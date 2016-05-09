1bab32aa59da8f4d4603b399e69d768e4477d92c
Arnold Wey <PandaStrawberry@users.noreply.github.com>
2016-05-09 12:16:59 -0400

    Initialized Regs to all 0's, MEM to all 1's iverilog -o vespa vespa.v

--- modules/vespa.v
+++ modules/vespa.v
@@ -403,9 +403,10 @@ module vespa;
 			end
 			`endif 	//TRACE_CC
 
-			`ifdef  TRACE_IR
+			`ifdef TRACE_IR
 			begin
 				$display("Instruction Register:%b",IR);
+			end
 			`endif 	//TRACE_IR
 
 			`ifdef TRACE_REGS
